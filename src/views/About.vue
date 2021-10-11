d4eb1ec5a88a0e63858e19eada309963bfe4245c
Ajuka David <ajukadavid@Ezinne.local>
2021-10-11 14:17:58 +0100

    initial commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque natus architecto ad mollitia esse eos delectus quod aspernatur veniam est eveniet, blanditiis quis dolorum quos hic reprehenderit iusto! Nam obcaecati architecto mollitia ipsam aliquid quibusdam voluptas nisi doloribus quos, itaque vel illo reprehenderit ad dolor, vero inventore. Sunt, voluptatibus voluptas aspernatur modi ad quisquam ipsam veritatis impedit repellat odit! Voluptatum odit omnis, non vero facere perspiciatis quae labore, sint molestiae placeat soluta dicta blanditiis ex minima doloribus sed quis in architecto asperiores maiores nam, id libero quasi. Itaque, accusantium porro, ea nesciunt alias nulla aperiam molestias commodi, veniam assumenda obcaecati?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque natus architecto ad mollitia esse eos delectus quod aspernatur veniam est eveniet, blanditiis quis dolorum quos hic reprehenderit iusto! Nam obcaecati architecto mollitia ipsam aliquid quibusdam voluptas nisi doloribus quos, itaque vel illo reprehenderit ad dolor, vero inventore. Sunt, voluptatibus voluptas aspernatur modi ad quisquam ipsam veritatis impedit repellat odit! Voluptatum odit omnis, non vero facere perspiciatis quae labore, sint molestiae placeat soluta dicta blanditiis ex minima doloribus sed quis in architecto asperiores maiores nam, id libero quasi. Itaque, accusantium porro, ea nesciunt alias nulla aperiam molestias commodi, veniam assumenda obcaecati?</p>
+
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>
+      Home Page
+    </h1>
+    <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed molestias velit, nesciunt ex aliquid ea animi accusamus neque modi natus facilis voluptatibus est, non fuga officiis minima nobis debitis? Vitae error, aliquam sit cum odio quae, laboriosam temporibus ut voluptates adipisci, sequi libero! Tenetur, ad dolor voluptatibus, explicabo, magnam a qui sit officiis cumque eaque asperiores quos non doloremque nulla ducimus aliquam? Sit non nostrum nulla deserunt ea commodi quae autem voluptas eaque, aut ab voluptatibus reprehenderit necessitatibus tempore vero fugiat, sapiente magnam nihil eos quam. Quibusdam est sed minima! Officiis, exercitationem quas. Rerum, unde ipsum sapiente quae qui impedit?  
+    </p>
+      <p>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sed molestias velit, nesciunt ex aliquid ea animi accusamus neque modi natus facilis voluptatibus est, non fuga officiis minima nobis debitis? Vitae error, aliquam sit cum odio quae, laboriosam temporibus ut voluptates adipisci, sequi libero! Tenetur, ad dolor voluptatibus, explicabo, magnam a qui sit officiis cumque eaque asperiores quos non doloremque nulla ducimus aliquam? Sit non nostrum nulla deserunt ea commodi quae autem voluptas eaque, aut ab voluptatibus reprehenderit necessitatibus tempore vero fugiat, sapiente magnam nihil eos quam. Quibusdam est sed minima! Officiis, exercitationem quas. Rerum, unde ipsum sapiente quae qui impedit?  
+    </p>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
